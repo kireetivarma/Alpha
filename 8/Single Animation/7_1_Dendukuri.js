4a6e808380755d91ec92e72b9c4dec11e23f24dd
Kireeti Varma <kireetivarma@users.noreply.github.com>
2018-07-20 03:21:17 +0530

    Change in directory names

---
+++ 8/Single Animation/7_1_Dendukuri.js
@@ -0,0 +1,6 @@
+function flip(event)
+{
+	var element = event.currentTarget;
+	/* Toggle the setting of the classname attribute */
+	element.className = (element.className == 'card') ? 'card flipped' : 'card';
+}
